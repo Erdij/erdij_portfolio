02dd909886fd49211e5f195c477310da453ec149
Erdij <yildirimsukruerdi@gmail.com>
2023-02-02 17:22:59 +0300

    AnimatedLetters_2

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,23 +1,57 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import E_letter from "../../assets/images/E_letter.png"
 import { Link } from "react-router-dom"
 import "./index.scss"
+import AnimatedLetters from "../AnimatedLetters"
 
 const Home = () => {
+  const [letterClass, setLetterClass] = useState("text-animate")
+  const nameArray = ["r", "d", "i", "j"]
+  const jobArray = [
+    "w",
+    "e",
+    "b",
+    " ",
+    "d",
+    "e",
+    "v",
+    "e",
+    "l",
+    "o",
+    "p",
+    "e",
+    "r",
+  ]
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass("text-animate-hover")
+    }, 4000)
+  }, [])
   return (
     <>
       <div className='container home-page '>
         <div className='text-zone'>
           <h1>
-            Hi, <br /> I'm
-            <img src={E_letter} alt='developer' />
-            rdij
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,</span>
             <br />
-            web developer
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+            <img src={E_letter} alt='developer' />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={15}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              idx={22}
+            />
           </h1>
           <h2>Frontend Developer / JavaScript / React.js</h2>
           <Link to='/contact' className='flat-button'>
-            {" "}
             CONTACT ME
           </Link>
         </div>
